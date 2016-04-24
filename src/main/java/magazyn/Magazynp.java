5de5dd6957e2048e9842347dadddfec528efbca8
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-04-24 22:28:12 +0200

    Update

--- src/main/java/magazyn/Magazynp.java
+++ src/main/java/magazyn/Magazynp.java
@@ -20,11 +20,17 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import lombok.*;
 
 /**
  *
  * @author xxbar
  */
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Magazynp {
 
     public Integer NR_MAG;
@@ -35,34 +41,11 @@ public class Magazynp {
     private String JEDN;
     private Double MASA;
     private java.sql.Date DATAD;
+    @Getter
     private static String[] propTym = {"NR_MAG", "NR_KARTY", "NR_ODPADU", "NR_KLIENTA", "FIRMA", "JEDN", "MASA", "DATAD"};
+    @Getter
     private static String[] colTym = {"NR_MAG", "NR_KARTY", "NR_ODPADU", "NR_KLIENTA", "FIRMA", "JEDN", "MASA", "DATAD"};
     private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
-//    private String DATAD;
-
-    public Magazynp() {
-        this.NR_MAG = null;
-        this.NR_KARTY = null;
-        this.NR_ODPADU = null;
-        this.NR_KLIENTA = null;
-        this.FIRMA = null;
-        this.JEDN = null;
-        this.MASA = null;
-        this.DATAD = null;
-    }
-
-    
-    
-    public Magazynp(Integer nr_mag, NumerKarty nr_karty, Integer nr_odpadu, Integer nr_klienta, Integer firma, String jedn, double masa, Date datad) {
-        this.NR_MAG = nr_mag;
-        this.NR_KARTY = nr_karty;
-        this.NR_ODPADU = nr_odpadu;
-        this.NR_KLIENTA = nr_klienta;
-        this.FIRMA = firma;
-        this.JEDN = jedn;
-        this.MASA = masa;
-        this.DATAD = datad;
-    }
 
     public Magazynp(String NR_MAG, String NR_KARTY, String NR_ODPADU, String NR_KLIENTA, String FIRMA, String JEDN, String MASA, String datad) {
         this.NR_MAG = Integer.parseInt(NR_MAG.replaceAll("[^\\d.]", ""));
@@ -81,7 +64,6 @@ public class Magazynp {
             Logger.getLogger(Magazynp.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Błąd: " + ex);
         }
-
     }
 
     public Magazynp(String row) {
@@ -124,99 +106,11 @@ public class Magazynp {
         }
 
         try {
-            
              this.DATAD = scan.hasNext() ? new Date(DATE_FORMAT.parse(scan.next()).getTime()) : null;
-                    
-//            this.DATAD = new Date(DATE_FORMAT.parse(scan.hasNext() ? scan.next() : null).getTime());
         } catch (ParseException ex) {
             this.DATAD = null;
             System.out.println("Błąd: " + ex);
         }
-    }
-
-    public Integer getNR_MAG() {
-        return NR_MAG;
-    }
-
-    public void setNR_MAG(Integer nr_mag) {
-        this.NR_MAG = nr_mag;
-    }
-
-    public NumerKarty getNR_KARTY() {
-        return NR_KARTY;
-    }
-
-    public void setNR_KARTY(NumerKarty NR_KARTY) {
-        this.NR_KARTY = NR_KARTY;
-    }
-
-    public Integer getNR_ODPADU() {
-        return NR_ODPADU;
-    }
-
-    public void setNR_ODPADU(Integer NR_ODPADU) {
-        this.NR_ODPADU = NR_ODPADU;
-    }
-
-    public Integer getNR_KLIENTA() {
-        return NR_KLIENTA;
-    }
-
-    public void setNR_KLIENTA(Integer NR_KLIENTA) {
-        this.NR_KLIENTA = NR_KLIENTA;
-    }
-
-    public Integer getFIRMA() {
-        return FIRMA;
-    }
-
-    public void setFIRMA(Integer FIRMA) {
-        this.FIRMA = FIRMA;
-    }
-
-    public String getJEDN() {
-        return JEDN;
-    }
-
-    public void setJEDN(String JEDN) {
-        this.JEDN = JEDN;
-    }
-
-    public double getMASA() {
-        return MASA;
-    }
-
-    public void setMASA(double MASA) {
-        this.MASA = MASA;
-    }
-
-    public Date getDATAD() {
-        return DATAD;
-    }
-
-    public void setDATAD(Date DATAD) {
-        this.DATAD = DATAD;
-    }
-
-    public static String[] getPropTym() {
-        return propTym;
-    }
-
-    public static void setPropTym(String[] propTym) {
-        Magazynp.propTym = propTym;
-    }
-
-    public static String[] getColTym() {
-        return colTym;
-    }
-
-    public static void setColTym(String[] colTym) {
-        Magazynp.colTym = colTym;
-    }
-
-    @Override
-    public String toString() {
-        return "Magazynp{" + "NR_MAG=" + NR_MAG + ", NR_KARTY=" + NR_KARTY + ", NR_ODPADU=" + NR_ODPADU + ", NR_KLIENTA=" + NR_KLIENTA + ", FIRMA=" + FIRMA + ", JEDN=" + JEDN + ", MASA=" + MASA + ", DATAD=" + DATAD + '}';
     }
 
     public static List<Magazynp> Open(Path path) {
